6f5dc85285ed479217c486d5044f0aaa0636994c
Alex Gonzalez <gonzo_rhcp@hotmail.com>
2018-04-09 11:10:34 +1200

    Added GpoBackupRestore.psm1

---
+++ Modules/GpoBackupRestore.psm1
@@ -0,0 +1,226 @@
+function Get-GpoPermission {
+    [CmdletBinding()]
+
+    param(
+        [Parameter(Mandatory=$true,ValueFromPipeline=$true,Position=0)] 
+        [Microsoft.GroupPolicy.Gpo]   
+        $InputObject
+    )
+
+    begin {
+    }
+    process {
+        [adsi]('LDAP://CN={' + $InputObject.Id + '},CN=Policies,CN=System,' + ([adsi]'LDAP://RootDSE').defaultNamingContext) |
+            Get-ADObjectAccessRight |
+            ForEach-Object {
+                $_.Parent_canonicalName = $_.Parent_canonicalName -replace("$($InputObject.Id)","$($InputObject.DisplayName)")
+                $_.Parent_distinguishedName = $_.Parent_distinguishedName -replace("$($InputObject.Id)","$($InputObject.DisplayName)")
+                $_
+            }
+    }
+    end {
+    } 
+}
+
+function Set-GpoPermission {
+    [CmdletBinding()]
+
+    param(
+        [Parameter(Mandatory=$true,ValueFromPipeline=$true,Position=0)] 
+        $InputObject,
+        [Parameter(Mandatory=$false,ValueFromPipeline=$false,Position=1)]
+        [switch]
+        $Force = $false
+    )
+
+    begin {
+    }
+    process {
+        $gpo = Get-GPO -DisplayName $InputObject.Parent_canonicalName.split('/')[-1].Trim('{}')
+        $_.Parent_canonicalName = $_.Parent_canonicalName -replace("$($gpo.DisplayName)","$($gpo.Id)")
+        $_.Parent_distinguishedName = $_.Parent_distinguishedName -replace("$($gpo.DisplayName)","$($gpo.Id)")
+        $_ | Set-ADObjectAccessRight -Force:($Force)
+    }
+    end {
+    } 
+}
+
+function Get-GpoLink {
+    [CmdletBinding()]
+
+    param(
+        [Parameter(Mandatory=$true,ValueFromPipeline=$true,Position=0)] 
+        [Microsoft.GroupPolicy.Gpo]   
+        $InputObject
+    )
+
+    begin {
+        [string[]]$organizationalUnits = Get-ADOrganizationalUnit -Filter * | 
+            Select-Object -ExpandProperty distinguishedName
+        $organizationalUnits += ([adsi]'LDAP://RootDSE').defaultNamingContext
+        $gpoLinks = $organizationalUnits |
+            Get-GPInheritance |
+            Select-Object -ExpandProperty GpoLinks
+        Remove-Variable -Name organizationalUnits
+    }
+    process {
+        $gpoLinks |
+            Where-Object {
+                $_.DisplayName -eq $InputObject.DisplayName
+            } |
+            ForEach-Object {
+                if ($_.Enabled) {$linkEnabled = 'Yes'} else {$linkEnabled = 'No'}
+                if ($_.Enforced) {$linkEnforced = 'Yes'} else {$linkEnforced = 'No'}
+                New-Object -TypeName psobject -Property @{
+                   'Id' = $_.GpoId
+                   'DisplayName' = $_.DisplayName
+                   'LinkOrder' = $_.Order
+                   'LinkEnabled' = $linkEnabled
+                   'LinkEnforced' = $linkEnforced
+                   'LinkTarget' = $_.Target
+                }
+            }
+    }
+    end {
+    } 
+}
+
+function Set-GpoLink {
+    [CmdletBinding()]
+
+    param(
+        [Parameter(Mandatory=$true,ValueFromPipeline=$true,Position=0)] 
+        $InputObject
+    )
+
+    begin {
+    }
+    process {
+            if (
+                (Get-ADOrganizationalUnit -Identity $InputObject.LinkTarget |
+                Get-GPInheritance |
+                Select-Object -ExpandProperty GpoLinks |
+                Select-Object -ExpandProperty DisplayName) -contains $InputObject.DisplayName
+            ) {
+                Set-GPLink -Name $_.DisplayName -Target $_.LinkTarget -Order $_.LinkOrder -LinkEnabled $_.LinkEnabled -Enforced $_.LinkEnforced 
+            }
+            else {
+                New-GPLink -Name $_.DisplayName -Target $_.LinkTarget -Order $_.LinkOrder -LinkEnabled $_.LinkEnabled -Enforced $_.LinkEnforced 
+            }
+
+    }
+    end {
+    }
+}
+function Backup-Gpo {
+    [CmdletBinding()]
+
+    param(
+        [Parameter(Mandatory=$true,ValueFromPipeline=$true,Position=0)] 
+        [Microsoft.GroupPolicy.Gpo]   
+        $InputObject,
+        [Parameter(Mandatory=$true,ValueFromPipeline=$false,Position=1)]
+        $Path
+    )
+
+    begin {
+        if (Test-Path -Path $Path) {
+            Write-Error -Message ("An item with the specified name '{0}' already exists." -f $Path)
+            break
+        }
+        $Path = New-Item -Force -ItemType Directory -Path $Path
+    }
+    process {
+        if ($InputObject.DisplayName.EndsWith(' ')) {
+            Write-Warning ("Not backing up policy '{0}' because it's DisplayName ends with a space (' ')." -f $InputObject.DisplayName)
+        }
+        elseif ($InputObject.DisplayName.StartsWith(' ')) {
+            Write-Warning ("Not backing up policy '{0}' because it's DisplayName starts with a space (' ')." -f $InputObject.DisplayName)
+        }
+        else {
+            $InputObject | 
+                GroupPolicy\Backup-GPO -Path $Path |
+                Export-Csv -Append -NoTypeInformation -Path (Join-Path -Path $Path -ChildPath 'policy.config.csv')
+            $InputObject |
+                Get-GpoPermission |
+                Export-Csv -Append -NoTypeInformation -Path (Join-Path -Path $Path -ChildPath 'ace.config.csv')
+            $InputObject | 
+                Get-GpoLink |
+                Export-Csv -Append -NoTypeInformation -Path (Join-Path -Path $Path -ChildPath 'link.config.csv')                
+        }
+    }
+    end {
+        Get-ADObject -SearchBase ('CN=Partitions,CN=Configuration,' + ([adsi]'LDAP://RootDSE').defaultNamingContext) -Filter * -Properties netbiosname | 
+            Select-Object -ExpandProperty netbiosname |
+            Out-File -FilePath (Join-Path -Path $Path -ChildPath 'netbiosname.config')
+    }
+}
+
+function Restore-Gpo {
+    [CmdletBinding()]
+
+    param(
+        [Parameter(Mandatory=$true,ValueFromPipeline=$false,Position=0)]
+        $Path,
+        [Parameter(Mandatory=$false,ValueFromPipeline=$false,Position=1)]
+        [string]
+        $MigrationTable=""
+    )
+
+    begin {
+        $netBiosName_source = Get-Content (Join-Path -Path $Path -ChildPath 'netbiosname.config').trim()
+        $netBiosName_target = Get-ADObject -SearchBase ('CN=Partitions,CN=Configuration,' + ([adsi]'LDAP://RootDSE').defaultNamingContext) -Filter * -Properties netbiosname | 
+            Select-Object -ExpandProperty netbiosname  
+    }
+    process {
+        Import-Csv -Path (Join-Path -Path $Path -ChildPath 'policy.config.csv') | 
+            ForEach-Object {
+                if ($MigrationTable.Length -gt 0) {
+                    GroupPolicy\Import-GPO -CreateIfNeeded -BackupGpoName $_.DisplayName -TargetName $_.DisplayName -Path $Path -MigrationTable $MigrationTable
+                } 
+                else {
+                    GroupPolicy\Import-GPO -CreateIfNeeded -BackupGpoName $_.DisplayName -TargetName $_.DisplayName -Path $Path
+                }
+            }
+
+        Import-Csv -Path (Join-Path -Path $Path -ChildPath 'ace.config.csv') | 
+            ForEach-Object {
+                $_.__AddRemoveIndicator = 1
+                $_.IdentityReference = $_.IdentityReference.ToUpper().replace("$($netBiosName_source)\","$($netBiosName_target)\")
+                $_.Parent_canonicalName = (([adsi]'LDAP://RootDSE').defaultNamingContext -replace('dc=','') -replace(',','.')) + $_.Parent_canonicalName.SubString($_.Parent_canonicalName.IndexOf('/'))
+                $_.Parent_distinguishedName = $_.Parent_distinguishedName.SubString(0,$_.Parent_distinguishedName.ToUpper().IndexOf('DC=')) + ([adsi]"LDAP://RootDSE").defaultNamingContext
+                $_ | Set-GpoPermission -Force
+            }
+
+        Import-Csv -Path (Join-Path -Path $Path -ChildPath 'link.config.csv') | 
+            ForEach-Object {        
+                $_.LinkTarget = $_.LinkTarget.SubString(0,$_.LinkTarget.ToUpper().IndexOf('DC=')) + ([adsi]"LDAP://RootDSE").defaultNamingContext
+                $_ | Set-GpoLink
+            }
+    }
+    end {
+    }
+}
+
+# This check is not ideal, need to replace with a propper .psd1 manifest
+if (-not (Get-Module -Name ActiveDirectory)) {
+    Import-Module ActiveDirectory
+}
+if (-not (Get-Module -Name GroupPolicy)) {
+    Import-Module GroupPolicy
+}
+if (-not (Get-Module -Name ADObjectAccessRight)) {
+    Write-Error -Message "This module requires the 'ADObjectAccessRight' module. Import the 'ADObjectAccessRight' module and then try to import this module again."
+    Export-ModuleMember
+    break
+}
+
+Export-ModuleMember `
+    -Function @(
+        'Get-GpoPermission',
+        'Set-GpoPermission',
+        'Get-GpoLink',
+        'Set-GpoLink',
+        'Backup-Gpo',
+        'Restore-Gpo'
+    )
