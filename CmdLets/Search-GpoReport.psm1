411e761cb52cf6f66faf5c722a5a433f11cd34db
Alex Gonzalez <gonzo_rhcp@hotmail.com>
2018-09-29 22:19:07 +1000

    Included Search-GpoReport to GroupPolicyEx module.

--- CmdLets/Search-GpoReport.psm1
+++ CmdLets/Search-GpoReport.psm1
@@ -1,16 +1,17 @@
-param (
-    [Parameter(Mandatory=$false,ValueFromPipeline=$true,Position=0)]    
-    [string]$Domain = (New-Object system.directoryservices.directoryentry).distinguishedname.tolower().replace('dc=','').replace(',','.'),
-    [Parameter(Mandatory=$true,ValueFromPipeline=$true,Position=1)]    
-    [string]$SearchString
-)
+function Search-GpoReport {
+    [CmdletBinding()]
 
-Import-Module GroupPolicy
+    param (
+        [Parameter(Mandatory=$false,ValueFromPipeline=$true,Position=0)]    
+        [string]$Domain = (New-Object system.directoryservices.directoryentry).distinguishedname.tolower().replace('dc=','').replace(',','.'),
+        [Parameter(Mandatory=$true,ValueFromPipeline=$true,Position=1)]    
+        [string]$SearchString
+    )
 
-foreach ($gpo in (Get-GPO -All -Domain $Domain)) {
-    Write-Host -ForegroundColor Green ("Searching: '{0}'..." -f $gpo.displayname)
+    foreach ($gpo in (Get-GPO -All -Domain $Domain)) {
+        Write-Host -ForegroundColor Green ("Searching: '{0}'..." -f $gpo.displayname)
 
-    ($gpo | Get-GPOReport -ReportType Html -Domain $Domain).split("`n") |
-        Select-String -CaseSensitive -Pattern $SearchString
+        ($gpo | Get-GPOReport -ReportType Html -Domain $Domain).split("`n") |
+            Select-String -CaseSensitive -Pattern $SearchString
+    }
 }
-
